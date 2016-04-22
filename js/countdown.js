1dfe2733cfc178a4f6e1ca2cb8b5092d61166930
Mariya Gnitetskaya <tennalian@gmail.com>
2016-04-22 14:29:53 +0200

    edit

--- js/countdown.js
+++ js/countdown.js
@@ -5,22 +5,18 @@ countdown.js
 
 var countdown = (function(elements){
 	'use strict';
-	var elements = elements;
-	console.log(elements);
-
-	var timer = function(element){
+	var timer = function(el){
 		var time;
-		if (element.dataset.time == undefined) {
+		if (el.dataset.time == undefined) {
 			time = Date.parse(new Date()) + 3600 * 1000 * 24 * 5; //5 days
 		}
 		else{
-			time = Date.parse(element.dataset.time);
+			time = Date.parse(el.dataset.time);
 		}
 		return time;
 	};
 
-	var counter = function(date, element){
-		console.log(element);
+	var counter = function(date, el){
  		var amount =  +date - Date.parse(new Date()),
 	        day = Math.floor(amount / 86400000),
 	        hours = Math.floor((amount / 3600000) % 24),
@@ -47,23 +43,22 @@ var countdown = (function(elements){
 			secsDiv = '<div id="secs">' + zero + '</div>';
  		}
         var out = daysDiv + hoursDiv + minsDiv + secsDiv ;
-        element.innerHTML = out;
+        el.innerHTML = out;
 	};
 
 	var parseElements = function(elements){
-		[].forEach.call(elements, function(element){
-			var date = timer(element);
+		[].forEach.call(elements, function(el){
+			var date = timer(el);
 			var self = this;
-			counter(date, element);
+			counter(date, el);
 			var interval = setInterval(function(){
-				counter(date, element);
+				counter(date, el);
 			},1000);
 			return interval;
 		});
 	}
 	return{
 		init: function(elements){
-			console.log('init');
 	       return parseElements(elements);
 	    }
 	}
